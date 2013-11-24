2efa26abee7031f5dabbcf2ac27043c39292a375
Kip Warner <kip@thevertigo.com>
2013-11-24 00:45:42 -0800

    Copying: Code should have been under GPLv3 or later... Handbook: Hundreds of edits, and hopefully the last for a long time... Handbook: Many new references...

---
+++ Handbook/References/validate_urls.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# validate_urls, to validate urls in a BibTeX database.
+# Copyright (C) 2010-2013 Cartesian Theatre <info@cartesiantheatre.com>.
+#
+# Public discussion on IRC available at #avaneya (irc.freenode.net) or
+# on the mailing list <avaneya@lists.avaneya.com>.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or 
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# System imports...
+
+# http://pybtex.sourceforge.net/manual.html
+
